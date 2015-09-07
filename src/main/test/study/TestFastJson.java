b3332fb295489a296f346b34f1f8ec6972b47f6c
renhengli518 <372594984@qq.com>
2015-09-07 19:50:37 +0800

    commit

--- src/main/test/study/TestFastJson.java
+++ src/main/test/study/TestFastJson.java
@@ -14,57 +14,52 @@ import com.springstudy.entity.User;
 /**
  * 
  * @author renhengli
- *
+ * 
  */
 public class TestFastJson {
-	public static void main (String [] args){
+	public static void main(String[] args) {
 		String test = "{'name':'张三','age':17}";
-		//将json字符串转成json对象
+		// 将json字符串转成json对象
 		JSONObject json = JSONObject.parseObject(test);
 		JSONObject json1 = JSON.parseObject(test);
 		System.out.println(json);
 		System.out.println(json1);
-		
-		//将json对象转成javabean
+
+		// 将json对象转成javabean
 		User user = JSON.toJavaObject(json1, User.class);
 		System.out.println(user);
-		
-		//将json字符串转成javabean
+
+		// 将json字符串转成javabean
 		User user1 = JSON.parseObject(test, User.class);
 		System.out.println(user1);
-		
-		//将javabean 对象转成Json 字符串
+
+		// 将javabean 对象转成Json 字符串
 		String str = JSON.toJSONString(user1);
 		System.out.println(str);
-		
-		//将javabean 对象转成json对象
+
+		// 将javabean 对象转成json对象
 		JSONObject json2 = (JSONObject) JSON.toJSON(user1);
 		System.out.println(json2);
+
+		// map转json
+		Map<String, String> hashMap = new HashMap<String, String>();
+		hashMap.put("name", "zhang");
+		hashMap.put("sex", "1");
+		hashMap.put("login", "Jack");
+		hashMap.put("password", "123abc");
+
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			String userMapJson = objectMapper.writeValueAsString(hashMap);
+			JsonNode node = objectMapper.readTree(userMapJson);
+			// 输出结果转意，输出正确的信息
+			System.out.println(node.get("password").asText());
+			// 输出不转意,输出结果会包含""，这是不正确的，除非作为json传递，如果是输出结果值，必须如上一行的操作
+			System.out.println(node.get("name"));
+		} catch (IOException e) {
 		
-		//map转json
-		Map<String, String> hashMap = new HashMap<String, String>();  
-		  hashMap.put("name", "zhang");  
-		  hashMap.put("sex", "1");  
-		  hashMap.put("login", "Jack");  
-		  hashMap.put("password", "123abc");  
-		  
-		  try  
-		  {  
-		    ObjectMapper objectMapper = new ObjectMapper();  
-		    String userMapJson = objectMapper.writeValueAsString(hashMap);  
-		  
-		    JsonNode node = objectMapper.readTree(userMapJson);  
-		  
-		    // 输出结果转意，输出正确的信息  
-		    System.out.println(node.get("password").asText());  
-		    // 输出不转意,输出结果会包含""，这是不正确的，除非作为json传递，如果是输出结果值，必须如上一行的操作  
-		    System.out.println(node.get("name"));  
-		  }  
-		  catch (IOException e)  
-		  {  
-		  }  
-		
-		
+		}
+
 	}
 
 }
